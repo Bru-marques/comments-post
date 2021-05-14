995025da2c9576f4a08202690f411aa3d5f6c1a4
Bruna Marques <brmarques1108@gmail.com>
2021-05-14 12:24:55 +0100

    edit comment started

--- index.js
+++ index.js
@@ -9,11 +9,13 @@ Destroy - DELETE /comment/:id - destroy one comment
 const express = require('express');
 const app = express();
 const path = require('path');
+const methodOverride = require('method-override')
 const { v4: uuid } = require('uuid');
 uuid();
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(methodOverride('_method'))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
@@ -62,14 +64,19 @@ app.get('/comments/:id', (req, res) => {
     res.render('comments/show', { comment })
 })
 
-/*app.get('/tacos', (req, res) => {
-    res.send("GET /tacos response");
-});
+app.get('/comments/:id/edit', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === id)
+    res.render('comments/edit', { comment })
+})
 
-app.post('/tacos', (req, res) => {
-    const { meat, qty } = req.body;
-    res.send(`Ok, here are your tacos`)
-});*/
+app.patch('comments/:id', (req, res) => {
+    const { id } = req.params;
+    const newCommentText = req.body.comment;
+    const foundComment = comments.find(c => c.id === id);
+    foundComment.comment = newCommentText;
+    res.redirect('/comments')
+})
 
 
 app.listen(3000, () => {
