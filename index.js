477ad9e0778783d5a4946e3273e61cf8f85f3833
Bruna Marques <brmarques1108@gmail.com>
2021-05-14 11:02:04 +0100

    new id end

--- index.js
+++ index.js
@@ -8,32 +8,34 @@ Destroy - DELETE /comment/:id - destroy one comment
 
 const express = require('express');
 const app = express();
-//const path = express('path');
+const path = require('path');
+const { v4: uuid } = require('uuid');
+uuid();
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-//app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 //Set comments
 const comments = [
     {
-        id: 1,
+        id: uuid(),
         username: 'Todd',
         comment: 'What a funny day!'
     },
     {
-        id: 2,
+        id: uuid(),
         username: 'Skyler',
         comment: 'I like walking with my dog.'
     },
     {
-        id: 3,
+        id: uuid(),
         username: 'Ben',
         comment: 'I am so tired  =('
     },
     {
-        id: 4,
+        id: uuid(),
         username: 'Happy People',
         comment: 'Good morning!'
     }
@@ -50,13 +52,13 @@ app.get('/comments/new', (req, res) => {
 
 app.post('/comments', (req, res) => {
     const { username, comment } = req.body;
-    comments.push({ username, comment });
+    comments.push({ username, comment, id: uuid() });
     res.redirect('/comments');
 })
 
 app.get('/comments/:id', (req, res) => {
     const { id } = req.params;
-    const comment = comments.find(c => c.id === parseInt(id))
+    const comment = comments.find(c => c.id === id)
     res.render('comments/show', { comment })
 })
 
