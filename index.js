d202effdf102cd452033f33e96ca272d9aae9921
Bruna Marques <brmarques1108@gmail.com>
2021-05-15 16:30:27 +0100

    included bootstrap

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ uuid();
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(methodOverride('_method'))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
