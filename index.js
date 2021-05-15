87ea97d6dc458d67001ff187603e4a3c04cfb1f9
Bruna Marques <brmarques1108@gmail.com>
2021-05-15 15:29:37 +0100

    functions finished

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ Destroy - DELETE /comment/:id - destroy one comment
 const express = require('express');
 const app = express();
 const path = require('path');
-const methodOverride = require('method-override')
+const methodOverride = require('method-override');
 const { v4: uuid } = require('uuid');
 uuid();
 
@@ -20,7 +20,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 //Set comments
-const comments = [
+let comments = [
     {
         id: uuid(),
         username: 'Todd',
@@ -43,39 +43,52 @@ const comments = [
     }
 ]
 
-
+//Show all comments
 app.get('/comments', (req, res) => {
     res.render('comments/index.ejs', { comments })
 });
 
+//Create a new comment
 app.get('/comments/new', (req, res) => {
     res.render('comments/new')
 })
 
+//Post new comment
 app.post('/comments', (req, res) => {
     const { username, comment } = req.body;
     comments.push({ username, comment, id: uuid() });
     res.redirect('/comments');
 })
 
+// Show a especific comment
 app.get('/comments/:id', (req, res) => {
     const { id } = req.params;
     const comment = comments.find(c => c.id === id)
     res.render('comments/show', { comment })
 })
 
+//Edit a comment
 app.get('/comments/:id/edit', (req, res) => {
     const { id } = req.params;
     const comment = comments.find(c => c.id === id)
     res.render('comments/edit', { comment })
 })
 
-app.patch('comments/:id', (req, res) => {
+//Salve edited comment
+app.patch('/comments/:id', (req, res) => {
     const { id } = req.params;
     const newCommentText = req.body.comment;
     const foundComment = comments.find(c => c.id === id);
     foundComment.comment = newCommentText;
     res.redirect('/comments')
+})
+
+
+//Delete a comment
+app.delete('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    comments = comments.filter(c => c.id !== id);
+    res.redirect('/comments');
 })
 
 
